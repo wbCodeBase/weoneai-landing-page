275f5ab1605dbbe7d23db2c9ac6fbe5eabff48a1
CoderKashyap <kashyap.abhi1105@gmail.com>
2025-07-28 14:24:37 +0530

    remove unused fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter, Roboto, Montserrat, Poppins, Raleway, Oswald } from "next/font/google";
+import { Geist, Geist_Mono, Raleway } from "next/font/google";
 import "./globals.css";
 
 
